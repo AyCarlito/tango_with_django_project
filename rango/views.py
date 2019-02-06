3034d556a9b2dc900ace112f629fd48b63e6a415
Daniel Hislop <2317990H@student.gla.ac.uk>
2019-02-06 23:04:53 +0000

    Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -7,21 +7,33 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 def index(request):
-
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
     page_list = Page.objects.order_by('-views')[:5]
-    context_dict['pages'] = page_list
+    context_dict = {'categories': category_list, 'pages': page_list}
 
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 
 def about(request):
-    context_dict = {'message': "This tutorial has been put together by Daniel Hislop"}
-    return render(request, 'rango/about.html', context=context_dict)
+    context_dict = {}
+    request.session.set_test_cookie()
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/about.html', context=context_dict)
+
+    return response
 
 def show_category(request, category_name_url):
     context_dict = {}
@@ -131,6 +143,28 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rango/login.html', {})
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
 
 @login_required
 def add_category(request):
